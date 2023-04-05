1f098114c5fd66330103361dc8e20fdb42f68158
Rémi Willebrouck <willremi@gmail.com>
2023-04-05 12:26:04 +0200

    stylisation de la présentation dans page compétences

--- Views/competences/index.php
+++ Views/competences/index.php
@@ -11,8 +11,11 @@
     <div class="tab-content mt-2" id="nav-tabContent">
         <div class="tab-pane fade show active mx-4" id="about" role="tabpanel" aria-labelledby="about-tab">
             <div class="row">
-                <div class="col-md-12 col-lg-6 bg-success">
-                    <p class="pt-2 pb-1">Je suis un développeur web qui aime la simplicité et l'élégance, j'aime créer selon les besoins des clients afin de leur proposer quelque chose au plus proche de leurs attentes. Lors du développement d'un site, je réffléchis d'un point de vue utilisateur afin de créer une interface agréable d'utilisation.</p>
+                <div class="col-md-12 col-lg-6 rounded" id="presentation">
+                    <p class="pt-2 px-2">Je me présente Rémi Willebrouck, j'ai <?= date('Y') - 1984 ?> ans. Je suis un développeur web qui aime la simplicité et l'élégance, j'aime créer selon les besoins des clients afin de leur proposer quelque chose au plus proche de leurs attentes. Lors du développement d'un site, je réffléchis d'un point de vue utilisateur afin de créer une interface agréable d'utilisation.</p>
+                    <a class="btn" href="mailto:wi11remi@wiremi.fr?subject=Message provenant du site 'Rémi W - Portfolio'">Me Contacter</a>
+                    <button class="btn">PDF</button>
+                    <br><br>
                 </div>
                 
             </div>
