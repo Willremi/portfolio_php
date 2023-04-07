c5d7849265d3f79a707da79b2f4dc9f4eae8acda
Rémi Willebrouck <willremi@gmail.com>
2023-04-07 20:15:53 +0200

    travail sur la partie technos dans page compétences

--- Views/competences/tabs/technos.php
+++ Views/competences/tabs/technos.php
@@ -1,1 +1,24 @@
-<div class="tab-pane fade" id="techno" role="tabpanel" aria-labelledby="techno-tab">Technos</div>
+<div class="tab-pane fade" id="techno" role="tabpanel" aria-labelledby="techno-tab">
+    <div class="row justify-content-center mx-auto">
+        <div class="col-9 rounded" id="technosDev">
+            <h2>Technologies en développement</h2>
+            <div id="ligne" class="py-2"></div>
+            <div class="">
+                <img src="/img/logos/html.png" alt="html">
+                <img src="/img/logos/css.png" alt="css">
+                <img src="/img/logos/javascript.png" alt="javascript">
+                <img src="/img/logos/bootstrap.png" alt="bootstrap">
+                <img src="/img/logos/jquery.png" alt="jquery">
+                <img src="/img/logos/react.png" alt="react">
+                <img src="/img/logos/tailwind.png" alt="tailwind">
+                <img src="/img/logos/php.png" alt="php">
+                <img src="/img/logos/node.png" alt="node">
+                <img src="/img/logos/mysql.png" alt="mysql">
+                <img src="/img/logos/mongodb.png" alt="mongodb">
+                <img src="/img/logos/symfony.png" alt="symfony">
+                <img src="/img/logos/sass.png" alt="sass">
+                <img src="/img/logos/wordpress.png" alt="wordpress">
+            </div>
+        </div> <!-- fin technosDev -->
+    </div> <!-- fin row -->
+</div>
