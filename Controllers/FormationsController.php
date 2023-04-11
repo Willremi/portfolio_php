0f2f4f2819d472d357dd43a3436f8cd572d9d057
Rémi Willebrouck <willremi@gmail.com>
2023-04-11 20:31:03 +0200

    travail sur la page formations

--- Controllers/FormationsController.php
+++ Controllers/FormationsController.php
@@ -6,6 +6,10 @@ class FormationsController extends Controller
 {
     public function index()
     {
-        $this->render('formations/index', [], 'default');
+        // Recherche dans le fichier json
+        $json = file_get_contents('js/datas/formations.json');
+        $formations = json_decode($json);
+
+        $this->render('formations/index', compact('formations'), 'default');
     }
 }
--- Views/formations/index.php
+++ Views/formations/index.php
@@ -1,4 +1,33 @@
 <?php $pageTitle = "Formations"; ?>
-<main id="competences" class="text-center rounded">
-    <h2><?= $pageTitle ?></h2>
+<main id="formations" class="text-center">
+    <div class="py-3">
+        <div class="row justify-content-center mx-auto">
+            <div class="col-10 col-md-9 mb-3 rounded" id="listForm">
+                <h2><?= $pageTitle ?></h2>
+                <div id="ligne" class="py-2"></div>
+
+                <div class="mb-3 justify-content-center row">
+                <?php foreach ($formations as $formation) : ?>
+                    <div class="col-md-10">
+                    <div class="accordion accordion-flush" id="accordionFlushExample">
+                        <div class="accordion-item">
+                            <h3 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?= $formation->id ?>" aria-expanded="false" aria-controls="flush-collapse<?= $formation->id ?>">
+                                    <?= $formation->title ?>
+                                </button>
+                            </h3>
+                            <div id="flush-collapse<?= $formation->id ?>" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <p class="text-dark"><?= $formation->date ?></p>
+                                <p class="text-dark"><?= $formation->lieu ?></p>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                <?php endforeach ?>
+                </div>
+
+            </div>
+        </div>
 </main>
