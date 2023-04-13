31c05b6c681edc83d02243ee9bd177eadb40f9f4
Rémi Willebrouck <willremi@gmail.com>
2023-04-13 09:33:27 +0200

    travail sur la page expériences 2

--- Views/experiences/index.php
+++ Views/experiences/index.php
@@ -7,14 +7,14 @@
                 <div id="ligne" class="py-2"></div>
                 <div class="row justify-content-center mx-auto">
                     <?php foreach ($experiences as $exp) : ?>
-                        <div class="col-6 col-md-4">
-                            <div class="card mb-2 text-dark">
+                        <div class="col-12 col-md-4">
+                            <div class="card mb-2">
                                 <div class="card-header">
                                     <h5><?= $exp->title ?></h5>
                                 </div>
                                 <div class="card-body" id="detailJob">
-                                    <span><?= $exp->date ?></span><br>
-                                    <span><?= $exp->lieu ?></span>
+                                    <span id="date"><?= $exp->date ?></span><br>
+                                    <span id="lieu"><?= $exp->lieu ?></span>
                                     <details>
                                         <summary>Activités</summary>
                                         <ul id="liste">
